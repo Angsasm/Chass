b2bab07a09a096e804ea5b7222dd08585b92995e
Angsasm <robertokrieb1@gmail.com>
2026-07-17 14:11:37 +0300

    Add files via upload

--- lichess-bot.py
+++ lichess-bot.py
@@ -3,15 +3,3 @@ from lib.lichess_bot import start_program
 
 if __name__ == "__main__":
     start_program()
-import http.server
-import socketserver
-import threading
-
-def run_dummy_server():
-    PORT = 10000
-    Handler = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        httpd.serve_forever()
-
-# Pornește serverul web pe un fir de execuție secundar
-threading.Thread(target=run_dummy_server, daemon=True).start()
